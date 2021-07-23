3b6de17cf653560ad94350c83151dcda65297154
William Cayetano <william@williamcayetano.com>
2021-07-23 15:03:50 -0700

    final push

---
+++ components/sandbox.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Sandbox() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>One</Text>
+            <Text style={styles.boxTwo}>Two</Text>
+            <Text style={styles.boxThree}>Three</Text>
+            <Text style={styles.boxFour}>Four</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        //flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 40,
+        backgroundColor: '#ddd'
+    },
+    boxOne: {
+        backgroundColor: 'violet',
+        padding: 10,
+    },
+    boxTwo: {
+        backgroundColor: 'gold',
+        padding: 10,
+    },
+    boxThree: {
+        backgroundColor: 'coral',
+        padding: 10,
+    },
+    boxFour: {
+        backgroundColor: 'skyblue',
+        padding: 10,
+    }
+});
